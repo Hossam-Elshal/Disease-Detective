e71a494b9af064cc84700d3255f62238c9ed0899
Hossam-Elshal <hossamelshal@std.mans.edu.eg>
2025-06-20 02:59:42 +0300

    second finalize

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,22 +1,22 @@
-import axios from "axios";
+// import axios from "axios";
 
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // ✅ خده من .env بدل ما يبقى ثابت
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const instance = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL, // ✅ خده من .env بدل ما يبقى ثابت
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-// ✨ Add token to every request automatically
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Token ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// // ✨ Add token to every request automatically
+// instance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) {
+//       config.headers.Authorization = `Token ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
-export default instance;
+// export default instance;
