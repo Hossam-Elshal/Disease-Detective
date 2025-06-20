703e58516e3710d828a055aee525a65933ecafa3
Hossam-Elshal <hossamelshal@std.mans.edu.eg>
2025-06-20 03:06:48 +0300

    third finalize

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [tailwindcss(),
-//     react()],
-// })
-
-
-
-// // vite.config.js
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
 //   server: {
